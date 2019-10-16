5a7a4defb2fee778419510168ada36a54b0c0618
JackDarkWeb <42289641+JackDarkWeb@users.noreply.github.com>
2019-10-16 14:44:37 +0300

    Update Controller.php
    Update

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -3,9 +3,16 @@
 
 class Controller extends DataBase
 {
+  private static $vars = [];
+
     static function CreateView($viewName, $data = []){
 
+         foreach($data as $key => $val){
+           self::$vars[$key] = $val;
+         }
+          extract(self::$vars);
+
         require_once("./Views/$viewName.php");
-        static::doSomeThing();
+        
     }
 }
